bd0be5e9ae4688c0075205f357c69fe492d92e21
CollinsEzeokeke <90806850+CollinsEzeokeke@users.noreply.github.com>
2026-02-13 03:55:16 +0100

    feat: Add `print_full_name` function and demonstrate its usage with an external variable.

--- user_input.rb
+++ user_input.rb
@@ -5,3 +5,19 @@
 # last_name = gets.chomp
 
 # puts "Hello #{first_name} #{last_name}! Welcome to the program!"
+
+# External Variable
+name = "Hello Universe"
+
+def print_full_name(first_name, last_name)
+    name = first_name + " " + last_name
+    puts "Your full name is #{name}"
+end
+
+print_full_name("Peter", "Jones")
+print_full_name("John", "Doe")
+print_full_name("Samuel", "Etoo")
+
+puts "Finally add in " + name
+
+puts "Program finished!!!"
