6f6b5cb34bb8ed815d5eb7eee31f4462a1e1831f
CollinsEzeokeke <90806850+CollinsEzeokeke@users.noreply.github.com>
2026-02-15 11:01:12 +0100

    Replace previous Ruby code examples with a new for loop demonstration.

--- user_input.rb
+++ user_input.rb
@@ -7,17 +7,79 @@
 # puts "Hello #{first_name} #{last_name}! Welcome to the program!"
 
 # External Variable
-name = "Hello Universe"
+# name = "Hello Universe"
 
-def print_full_name(first_name, last_name)
-    name = first_name + " " + last_name
-    puts "Your full name is #{name}"
+# def print_full_name(first_name, last_name)
+#     name = first_name + " " + last_name
+#     puts "Your full name is #{name}"
+# end
+
+# print_full_name("Peter", "Jones")
+# print_full_name("John", "Doe")
+# print_full_name("Samuel", "Etoo")
+
+# puts "Finally add in " + name
+
+# puts "Program finished!!!"
+
+
+# total = 0
+# [1, 2, 3].each { |number| total += number }
+# puts total # 6
+
+
+# # scope.rb
+
+# a = 5             # variable is initialized in the outer scope
+
+# 3.times do |n|    # method invocation with a block
+#   a = 3           # is a accessible here, in an inner scope?
+# end
+
+# puts a
+
+# a = 5
+# 5.times do |n|
+#   puts "Inside block: n=#{n}, a=#{a}"
+#   a = 3
+# end
+# puts "Outside: #{a}"
+
+
+# a = 5
+# 5.times do |n|
+#   puts "Iteration #{n}: a is currently #{a}"
+#   a = 3
+# end
+# puts "Final: #{a}"
+
+a = 10
+
+# # 3.times {|number| a += number}
+# # puts a
+
+# 3.times do |number| 
+#     puts "this is waht the number is #{number}"
+#     a *= number
+# end
+
+# puts a
+
+# 3.times do |number|
+#     puts "this is the second section!!"
+#     a = 3
+#     b = 4
+# end
+
+# puts a
+# # puts b
+
+
+arr = [1, 2, 3]
+
+for i in arr do
+    puts "How many times does the loop run? #{i}"
+    a *= i       # a is initialized here
 end
 
-print_full_name("Peter", "Jones")
-print_full_name("John", "Doe")
-print_full_name("Samuel", "Etoo")
-
-puts "Finally add in " + name
-
-puts "Program finished!!!"
+puts a       # is it accessible here?
