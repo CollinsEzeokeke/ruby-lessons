fdca808ebe2c7df478c5f197a8513e139df7a1c6
CollinsEzeokeke <collinsmecosy@gmail.com>
2026-02-18 14:28:27 +0100

    feat: Enhance print functionality in script by adding multiple print statements and user input handling.

--- print.rb
+++ print.rb
@@ -3,3 +3,11 @@ print "1234567890"
 
 x = "My name is Collins Chidubem!!"
 puts x
+
+ print "Hello World "; print "I love drinking coffee"
+
+ puts "Hello World"; puts "I love drinking coffee"
+
+ new_string = gets.chomp
+
+ puts new_string
