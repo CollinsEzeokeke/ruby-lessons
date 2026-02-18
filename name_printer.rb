22ebee04da1a6e80db8e7efc14833acc93177b38
CollinsEzeokeke <collinsmecosy@gmail.com>
2026-02-18 13:17:46 +0100

    fix: Refine age calculation logic in user input script to improve accuracy and handle edge cases.

---
+++ name_printer.rb
@@ -0,0 +1,9 @@
+puts "Please enter your name: "
+name = gets.chomp
+
+# Print the name 10 times
+10.times do
+    puts "Hello #{name}! Welcome to the program!"
+end
+
+puts "Thank you #{name} for using the name printer! Have a great day!"
