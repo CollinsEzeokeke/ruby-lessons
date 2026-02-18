d192a45b644bd6c23b2c981b9930a8869d31f6f9
CollinsEzeokeke <collinsmecosy@gmail.com>
2026-02-18 15:51:46 +0100

    chore: Add comments and additional print statements to enhance clarity and provide context in print.rb

--- print.rb
+++ print.rb
@@ -11,3 +11,6 @@ puts x
  new_string = gets.chomp
 
  puts new_string
+# Hello World
+#the work we've put in here isn't easay at all so we keep on working nonetheless
+# Hello world this is magnificent#This world is magnificient# I think that this is wonderful and perfect in every single way this is just perfect and very very nice in every single way possible # How many lines do you think that all these can bring about I guess only one extra line push or two i think 
---
+++ reading_files.rb
@@ -0,0 +1,33 @@
+
+aFile = File.new("user_input.rb", "r")
+if aFile
+   content = aFile.sysread(20)
+   puts content
+else
+   puts "Unable to open file!"
+end
+
+age = File.new("age.rb", "r")
+
+if age
+    content = age.sysread(age.stat.size)
+    puts content
+else
+    puts "Error: Could not open file"
+end
+
+printFile = File.new("input.txt", "r+")
+
+if printFile
+    content = printFile.sysread(printFile.stat.size)
+    puts content
+    
+    puts "Enter a string to write to the file: "
+    puts "--------------------------------"
+    content2 = printFile.syswrite(gets)
+    puts content2
+    puts "File written successfully"
+    printFile.close()
+else
+    puts "Error: Could not open file"
+end
